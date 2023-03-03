1b38cfef7b0760185db3cb6b79352a6dd887d04d
TinaByrd <thuaneoliveira1@gmail.com>
2023-03-03 10:45:16 -0300

    "resolvendo o exercício de nota

---
+++ exercicio-estrutura-de-repeticao/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/* Faça um programa que peça uma nota, entre zero e dez. 
+Mostre uma mensagem caso o valor seja inválido e continue pedindo 
+até que o usuário informe um valor válido. */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira a nota do aluno");
+            double nota = scanner.nextDouble();
+        
+        while (nota > 10) {
+            System.out.println("Insira um valor válido");
+                nota = scanner.nextDouble();
+        }
+    }
+}
